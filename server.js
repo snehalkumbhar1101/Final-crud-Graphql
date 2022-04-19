0d977ff6e7c9dda3fd6b0066b830c7df6701045d
snehal kumbhar <99393067+snehalkumbhar1101@users.noreply.github.com>
2022-04-19 10:35:39 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,30 @@
+const { ApolloServer, gql } = require('apollo-server');
+const fs = require('fs');
+
+const resolvers = require('./graphql/resolvers');
+const typeDefs =  gql(fs.readFileSync('./graphql/typeDefs.graphql', {encoding: 'utf-8'}));
+
+const server = new ApolloServer({
+    typeDefs, resolvers,
+    context: ({ req }) => {
+        const token = req.headers.authorization || '';
+
+        const employee = getEmployeeDetails(token);
+        if (!getEmployeeDetails) throw new authorizationError('You must be logged in')
+
+        return {
+            EmployeeDetails,
+            models: {
+                getEmployeeDetails: generateemployeeModel({ getEmployeeDetails }),
+            }
+
+        }
+        },
+   
+});
+
+server.listen(5000).then(({
+  url
+}) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
